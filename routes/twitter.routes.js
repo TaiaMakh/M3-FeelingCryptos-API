c234ee63ad256ecb29c36215d70c28a733a71404
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-13 18:35:39 +0200

    Implement twitter route

--- routes/twitter.routes.js
+++ routes/twitter.routes.js
@@ -2,15 +2,8 @@ const express = require("express");
 const router = express.Router();
 const client = require('../configs/twitter.config');
 
-var params = {
-	"query": "#doge",
-    "max_results": 10,
-    "tweet.fields": ["public_metrics","author_id"],
-};
-
-router.get("/tweets", async (req, res, next) => {
-    //const { data } = await client.get('tweets', { ids: '1228393702244134912' });
-    const {data} = await client.get('tweets/search/recent', params);
+router.post("/recentTweets", async (req, res, next) => {
+    const {data} = await client.get('tweets/search/recent', req.body);
     return res.status(200).json(data);
 });
 
