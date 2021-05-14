4b646fce2ea9edc7f9767f9623982bee398af4c8
Silinde87 <paurodriguezmolina@gmail.com>
2021-05-14 13:23:56 +0200

    Enable twitter bearer token

--- configs/twitter.config.js
+++ configs/twitter.config.js
@@ -3,9 +3,9 @@ const Twitter = require('twitter-v2');
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    //bearer_token: process.env.TWITTER_BEARER_TOKEN,
+    // access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    // access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN,
 });
 
 module.exports = client;
